923f404baed3bc15e0b10c0b1bb9736adfc15016
Louis Trezzini <louis@feedly.com>
2020-10-11 23:01:42 +0200

    Query next trams (hardcoded station for now)

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,17 +1,27 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:zvv_next/models/connection.dart';
 import 'package:zvv_next/models/station_info.dart';
+import 'package:zvv_next/services/zvv_service.dart';
 
 import 'station_card.dart';
 
+var XXX = StationInfo(
+    "A=1@O=Zürich, Salersteig@X=8548481@Y=47406071@U=85@L=008591332@B=1@p=1601975100@",
+    "Zürich, Salersteig");
+
 class HomePage extends StatefulWidget {
+  final ZvvService zvvService;
+
+  HomePage({this.zvvService});
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   final List<StationInfo> _stations = [];
+  final Map<StationInfo, List<Connection>> _connections = {};
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +33,8 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.all(8),
           itemCount: _stations.length,
           itemBuilder: (context, index) {
-            return StationCard(_stations[index]);
+            var station = _stations[index];
+            return StationCard(station, connections: _connections[station]);
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: _addStation,
@@ -33,10 +44,17 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _addStation() {
-    log("clicked");
+  void _addStation() async {
+    // log("clicked");
+    // setState(() {
+    //   _stations.add(StationInfo((_stations.length + 1).toString(), "Salersteig ${_stations.length + 1}"));
+    // });
+    var x = await this.widget.zvvService.getStationTimetable(XXX);
+    print(x);
     setState(() {
-      _stations.add(StationInfo((_stations.length + 1).toString(), "Salersteig ${_stations.length + 1}"));
+      _stations.clear();
+      _stations.add(XXX);
+      _connections[XXX] = x;
     });
   }
 }
